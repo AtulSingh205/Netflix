a6117a52a3afcff1cc3d2290fb31beb6ce20b132
AtulSingh205 <atulsingh07062004@gmail.com>
2025-09-23 23:44:41 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Sbkamaliek from './Context/Sbkamaliek.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Sbkamaliek>
+ <App />
+    </Sbkamaliek>
+   
   </StrictMode>,
 )
